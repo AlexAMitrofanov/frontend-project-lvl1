7d5d3cc603ece559688670f9d8e68f3db99154d2
Alex Mitrofanov <alexloves.114@gmail.com>
2020-07-29 18:51:01 +0300

    fixed lint errors in brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -14,25 +14,23 @@ const igra = (a) => {
   if (a === 3) {
     return console.log(`Congratulations, ${name}!`);
   }
+
   const number = Math.floor(Math.random() * 100);
   console.log(`Question: ${number}`);
   const answ = readlineSinc.question('Your answer: ');
+
   if ((answ === 'yes' && number % 2 === 0) || (answ === 'no' && number % 2 !== 0)) {
     console.log('correct');
     return igra(a + 1);
   }
   if (answ === 'yes' && number % 2 !== 0) {
-    console.log('"yes" is the wrong answer ;(. Correct answer was "no".');
-    console.log(`Let's try again, ${name}!`);
+    return console.log(`"yes" is the wrong answer ;(. Correct answer was "no".\nLet's try again, ${name}!`);
   }
   if (answ === 'no' && number % 2 === 0) {
-    console.log('"no" is the wrong answer ;(. Correct answer was "yes".');
-    console.log(`Let's try again, ${name}!`);
+    return console.log(`"no" is the wrong answer ;(. Correct answer was "yes".\nLet's try again, ${name}!`);
   }
-  if (answ !== 'yes' && answ !== 'no') {
-    console.log('This is the wrong answer.');
-    console.log(`Let's try again, ${name}!`);
-  }
+
+  return console.log(`This is the wrong answer.\nLet's try again, ${name}!`);
 };
 
 igra(0);
