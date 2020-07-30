4a8efb0f07ee542da53163424c660e90b77decd2
Alex Mitrofanov <alexloves.114@gmail.com>
2020-07-30 20:06:15 +0300

    fixed errors brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -14,45 +14,31 @@ const igra = (a) => {
   if (a === 3) {
     return console.log(`Congratulations, ${name}!`);
   }
-  let answ;
+  let quest;
   const number1 = Math.floor(Math.random() * 100);
   const number2 = Math.floor(Math.random() * 10);
   const det = Math.random();
+
   if (det <= 0.3333) {
+    quest = number1 + number2;
     console.log(`Question: ${number1} + ${number2}`);
-    answ = readlineSinc.question('Your answer: ');
-    if (answ === `${number1 + number2}`) {
-      console.log('correct');
-      return igra(a + 1);
-    }
-    if (answ !== `${number1 + number2}`) {
-      return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${number1 + number2}".\nLet's try again, ${name}!`);
-    }
   }
   if (det > 0.3333 && det <= 0.6666) {
+    quest = number1 - number2;
     console.log(`Question: ${number1} - ${number2}`);
-    answ = readlineSinc.question('Your answer: ');
-    if (answ === `${number1 - number2}`) {
-      console.log('correct');
-      return igra(a + 1);
-    }
-    if (answ !== `${number1 - number2}`) {
-      return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${number1 - number2}".\nLet's try again, ${name}!`);
-    }
   }
   if (det > 0.6666) {
+    quest = number1 * number2;
     console.log(`Question: ${number1} * ${number2}`);
-    answ = readlineSinc.question('Your answer: ');
-    if (answ === `${number1 * number2}`) {
-      console.log('correct');
-      return igra(a + 1);
-    }
-    if (answ !== `${number1 - number2}*`) {
-      return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${number1 * number2}".\nLet's try again, ${name}!`);
-    }
   }
 
-  return console.log(`This is the wrong answer.\nLet's try again, ${name}!`);
+  const answ = readlineSinc.question('Your answer: ');
+
+  if (`${quest}` === answ) {
+    console.log('Correct!');
+    return igra(a + 1);
+  }
+  return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${number1 * number2}".\nLet's try again, ${name}!`);
 };
 
 igra(0);
