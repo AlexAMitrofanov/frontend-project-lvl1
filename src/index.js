da368424a18b95c45ba8abf1fa79c31023e9c1d6
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-03 18:36:32 +0300

    fixed game logic

--- src/index.js
+++ src/index.js
@@ -8,32 +8,24 @@ const name = readlineSinc.question('May I have your name? ');
 
 console.log(`Hello, ${name}!`);
 
-export const igraCalc = (b) => {
-  if (b === 0) {
-    console.log('What is the result of the expression?');
-  }
-  if (b === 3) {
+const igraLogic = (b, f) => {
+  const r = f();
+  if (b === 2) {
     return console.log(`Congratulations, ${name}!`);
   }
-  const ig = brCalc();
-  if (ig === 'Correct!') {
+  if (r === 'Correct!') {
     console.log('Correct!');
-    return igraCalc(b + 1);
+    return igraLogic(b + 1, f);
   }
   return console.log(`Let's try again, ${name}!`);
 };
 
-export const igraEv = (b) => {
-  if (b === 0) {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  }
-  if (b === 3) {
-    return console.log(`Congratulations, ${name}!`);
-  }
-  const ig = brEv();
-  if (ig === 'Correct!') {
-    console.log('Correct!');
-    return igraEv(b + 1);
-  }
-  return console.log(`Let's try again, ${name}!`);
+export const igraCalc = () => {
+  console.log('What is the result of the expression?');
+  return igraLogic(0, brCalc);
+};
+
+export const igraEv = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  return igraLogic(0, brEv);
 };
