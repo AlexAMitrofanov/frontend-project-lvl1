12e7d2343d4642cf272dca131dd9749a334c52f7
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-07 20:22:43 +0300

    renamed variable

--- src/games/brainProgGame.js
+++ src/games/brainProgGame.js
@@ -6,27 +6,24 @@ const brProg = () => {
   const step = Math.ceil(Math.random() * 100);
   const posElem = Math.floor(Math.random() * 10);
 
-  let a = number;
+  let nextNumber = number;
   let str = '';
   for (let i = 0; i < 10; i += 1) {
     if (i === posElem) {
-      a += step;
+      nextNumber += step;
       str += '.. ';
-      quest = a;
+      quest = nextNumber;
     }
     if (i !== posElem) {
-      a += step;
-      str = `${str}${a} `;
+      nextNumber += step;
+      str = `${str}${nextNumber} `;
     }
   }
   console.log(`Question: ${str}`);
 
   const answ = readlineSinc.question('Your answer: ');
 
-  if (`${quest}` === answ) {
-    return 'Correct!';
-  }
-  return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${quest}".`);
+  return (`${quest}` === answ) ? 'Correct!' : console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${quest}".`);
 };
 
 export default brProg;
