5195965aab48aa5cc7e075c86288564467f04f84
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-07 17:50:43 +0300

    create brainPrimeGame.js

---
+++ src/games/brainPrimeGame.js
@@ -0,0 +1,14 @@
+import readlineSinc from 'readline-sync';
+
+const brPrime = () => {
+  let quest;
+
+  const answ = readlineSinc.question('Your answer: ');
+
+  if (`${quest}` === answ) {
+    return 'Correct!';
+  }
+  return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${quest}".`);
+};
+
+export default brPrime;
