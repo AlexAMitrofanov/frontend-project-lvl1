df15b7b1929e79183f9b36c491181c7a3a2cfcc9
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-07 18:23:26 +0300

    add code

--- src/games/brainPrimeGame.js
+++ src/games/brainPrimeGame.js
@@ -2,6 +2,18 @@ import readlineSinc from 'readline-sync';
 
 const brPrime = () => {
   let quest;
+  const number = Math.ceil(Math.random() * 100);
+  const half = Math.floor(number / 2);
+  let counter = 0;
+  if (number === 1) {
+    quest = 'no';
+  }
+  for (let i = half; i >= 0; i -= 1) {
+    counter = (number % i === 0) ? counter + 1 : counter;
+  }
+  quest = (counter === 1) ? 'yes' : 'no';
+
+  console.log(`Question: ${number}`);
 
   const answ = readlineSinc.question('Your answer: ');
 
