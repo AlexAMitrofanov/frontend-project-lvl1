88d9b6dd04ea507f946a5bc5e3063deb1afa518d
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-13 19:40:40 +0300

    add number func import

--- src/games/brainGcdGame.js
+++ src/games/brainGcdGame.js
@@ -1,15 +1,16 @@
 import readlineSinc from 'readline-sync';
+import { number } from '../cli.js';
 
 const brGcd = () => {
   let quest;
-  const number = Math.ceil(Math.random() * 100);
-  const number1 = Math.ceil(Math.random() * 100);
-  console.log(`Question: ${number} ${number1}`);
+  const roundNumber = number();
+  const roundNumber1 = number();
+  console.log(`Question: ${roundNumber} ${roundNumber1}`);
   let a;
   let b;
-  for (let i = number1; i > 0; i -= 1) {
-    a = number % i;
-    b = number1 % i;
+  for (let i = roundNumber1; i > 0; i -= 1) {
+    a = roundNumber % i;
+    b = roundNumber1 % i;
     if (a === 0 && b === 0) {
       quest = i;
       break;
