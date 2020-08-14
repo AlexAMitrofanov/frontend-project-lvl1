b8825248687a9ea89a2e96f2e23d27aed614e08f
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-14 19:08:41 +0300

    renamed igraLogic to gameLogic

--- src/index.js
+++ src/index.js
@@ -11,39 +11,39 @@ const name = readlineSinc.question('May I have your name? ');
 
 console.log(`Hello, ${name}!`);
 
-const igraLogic = (b, f) => {
+const gameLogic = (b, f) => {
   const returnFunc = f();
   if (b === 2) {
     return console.log(`Congratulations, ${name}!`);
   }
   if (returnFunc === 'Correct!') {
     console.log('Correct!');
-    return igraLogic(b + 1, f);
+    return gameLogic(b + 1, f);
   }
   return console.log(`Let's try again, ${name}!`);
 };
 
 export const gameCalc = () => {
   console.log('What is the result of the expression?');
-  return igraLogic(0, brCalc);
+  return gameLogic(0, brCalc);
 };
 
 export const gameEv = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  return igraLogic(0, brEv);
+  return gameLogic(0, brEv);
 };
 
 export const gameGcd = () => {
   console.log('Find the greatest common divisor of given numbers.');
-  return igraLogic(0, brGcd);
+  return gameLogic(0, brGcd);
 };
 
 export const gameProg = () => {
   console.log('What number is missing in the progression?');
-  return igraLogic(0, brProg);
+  return gameLogic(0, brProg);
 };
 
 export const gamePrime = () => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
-  return igraLogic(0, brPrime);
+  return gameLogic(0, brPrime);
 };
