d4e6ffe80874038d2bbe4ebb72f9a2d61a46c2cf
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-14 19:58:48 +0300

    fixed open/closed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import readlineSinc from 'readline-sync';
-import brCalc from './games/brainCalGame.js';
-import brEv from './games/brainEvGame.js';
-import brGcd from './games/brainGcdGame.js';
-import brProg from './games/brainProgGame.js';
-import brPrime from './games/brainPrimeGame.js';
 
 console.log('Welcome to the Brain Games');
 
@@ -23,27 +18,4 @@ const gameLogic = (b, f) => {
   return console.log(`Let's try again, ${name}!`);
 };
 
-export const gameCalc = () => {
-  console.log('What is the result of the expression?');
-  return gameLogic(0, brCalc);
-};
-
-export const gameEv = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  return gameLogic(0, brEv);
-};
-
-export const gameGcd = () => {
-  console.log('Find the greatest common divisor of given numbers.');
-  return gameLogic(0, brGcd);
-};
-
-export const gameProg = () => {
-  console.log('What number is missing in the progression?');
-  return gameLogic(0, brProg);
-};
-
-export const gamePrime = () => {
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
-  return gameLogic(0, brPrime);
-};
+export default gameLogic;
