41778b68b0997a5de5d0db5fde005b3f8c963f67
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-22 10:17:17 +0300

    fixed open-closed principle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSinc from 'readline-sync';
 
-const gameLogic = (f, toDo) => {
+const gameLogic = (f, toDo, question) => {
   console.log('Welcome to the Brain Games');
 
   const name = readlineSinc.question('May I have your name? ');
@@ -12,13 +12,19 @@ const gameLogic = (f, toDo) => {
     if (b > 2) {
       return console.log(`Congratulations, ${name}!`);
     }
-    const returnFunc = f();
-    if (returnFunc === 'Correct!') {
+    const question1 = question();
+    console.log(question1);
+    const roundQuestion = question1.split('  ')[1];
+    const returnFunc = f(roundQuestion);
+    const playersAnsw = readlineSinc.question('Your answer: ');
+    if (returnFunc === playersAnsw) {
       console.log('Correct!');
       return iter(b + 1, func);
     }
-    return console.log(`Let's try again, ${name}!`);
+    return console.log(`"${playersAnsw}" is the wrong answer ;(. Correct answer was "${returnFunc}".
+Let's try again, ${name}!`);
   };
+
   return iter(0, f);
 };
 
