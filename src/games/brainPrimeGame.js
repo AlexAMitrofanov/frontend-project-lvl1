3fb5b928a9d75cf5b8078eecdc7d6b26c5a5d85d
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-22 10:19:45 +0300

    fixed open-closed principle

--- src/games/brainPrimeGame.js
+++ src/games/brainPrimeGame.js
@@ -1,28 +1,21 @@
-import readlineSinc from 'readline-sync';
 import { number } from '../cli.js';
 import gameLogic from '../index.js';
 
-const brPrime = () => {
+const question = () => `Question:  ${number()}`;
+
+const brPrime = (a) => {
   let counter = 0;
-  const roundNumber = number();
-  for (let i = roundNumber; i >= 2; i -= 1) {
-    counter = (roundNumber % i === 0) ? counter + 1 : counter;
+  for (let i = a; i >= 2; i -= 1) {
+    counter = (a % i === 0) ? counter + 1 : counter;
   }
-  const prime = 1;
-  const quest = (counter === prime) ? 'yes' : 'no';
-
-  console.log(`Question: ${roundNumber}`);
-
-  const answ = readlineSinc.question('Your answer: ');
-
-  if (`${quest}` === answ) {
-    return 'Correct!';
+  if (counter === 1) {
+    return 'yes';
   }
-  return console.log(`"${answ}" is the wrong answer ;(. Correct answer was "${quest}".`);
+  return 'no';
 };
 
 const whatTodo = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
-const gamePrime = () => gameLogic(brPrime, whatTodo);
+const gamePrime = () => gameLogic(brPrime, whatTodo, question);
 
 export default gamePrime;
