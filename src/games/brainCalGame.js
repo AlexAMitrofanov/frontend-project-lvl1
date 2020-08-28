b7fd5186dfdcadbeacc7647902fca4c668f35ae3
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-28 19:28:46 +0300

    fixed magic numbers

--- src/games/brainCalGame.js
+++ src/games/brainCalGame.js
@@ -2,32 +2,33 @@ import { number } from '../cli.js';
 import gameLogic from '../index.js';
 
 const sign = () => {
-  const determ = Math.floor(Math.random() * 3);
+  const operators = '+-*';
+  const determ = Math.floor(Math.random() * operators.length);
   const addition = 0;
   const substaction = 1;
   if (determ === addition) {
-    return '+';
+    return operators[0];
   }
   if (determ === substaction) {
-    return '-';
+    return operators[1];
   }
-  return '*';
+  return operators[2];
 };
 const question = () => `${number()} ${sign()} ${number()}`;
 
 const brCalc = (mass) => {
   let quest;
 
-  const num1 = +mass[1];
-  const num2 = +mass[3];
+  const num1 = +mass[0];
+  const num2 = +mass[2];
 
-  if (mass[2] === '+') {
+  if (mass[1] === '+') {
     quest = num1 + num2;
   }
-  if (mass[2] === '-') {
+  if (mass[1] === '-') {
     quest = num1 - num2;
   }
-  if (mass[2] === '*') {
+  if (mass[1] === '*') {
     quest = num1 * num2;
   }
   return quest;
