b2f152922fbde13cc610001ca10cd70a11073f03
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-29 16:39:19 +0300

    renamed functions

--- src/games/brainGcdGame.js
+++ src/games/brainGcdGame.js
@@ -1,31 +1,36 @@
 import { number } from '../cli.js';
-import gameLogic from '../index.js';
+import playGame from '../index.js';
 
-const firstNum = () => number() + 1;
-const secondNum = () => number() + 1;
-
-const question = () => `${firstNum()} ${secondNum()}`;
-
-const brGcd = (mass) => {
-  const num = mass[0];
-  const num1 = mass[1];
-  let quest;
-
+const getDivisor = (mass) => {
+  const num = +mass[0];
+  const num1 = +mass[1];
+  let result;
   let devider1;
   let devider2;
   for (let i = num; i > 0; i -= 1) {
     devider1 = num % i;
     devider2 = num1 % i;
     if (devider1 === 0 && devider2 === 0) {
-      quest = i;
+      result = i;
       break;
     }
   }
-  return quest;
+  return result;
 };
 
-const whatTodo = 'Find the greatest common divisor of given numbers.';
+const getAnswAndQuest = () => {
+  const firstNum = () => number() + 1;
+  const secondNum = () => number() + 1;
+  const question = () => `${firstNum()} ${secondNum()}`;
+  const questionForPlayer = question();
+  const questionForTransmission = questionForPlayer.split(' ');
+  const answer = getDivisor(questionForTransmission);
+  const result = [answer, questionForPlayer];
+  return result;
+};
 
-const gameGcd = () => gameLogic(brGcd, whatTodo, question);
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gameGcd = () => playGame(getAnswAndQuest, description);
 
 export default gameGcd;
