28b206b28713ecee9e114e7d87d3dc424133f903
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-29 17:26:24 +0300

    fixed getDisisor func

--- src/games/brainGcdGame.js
+++ src/games/brainGcdGame.js
@@ -1,15 +1,13 @@
 import { number } from '../cli.js';
 import playGame from '../index.js';
 
-const getDivisor = (mass) => {
-  const num = +mass[0];
-  const num1 = +mass[1];
+const getDivisor = (a, b) => {
   let result;
   let devider1;
   let devider2;
-  for (let i = num; i > 0; i -= 1) {
-    devider1 = num % i;
-    devider2 = num1 % i;
+  for (let i = a; i > 0; i -= 1) {
+    devider1 = a % i;
+    devider2 = b % i;
     if (devider1 === 0 && devider2 === 0) {
       result = i;
       break;
@@ -19,13 +17,12 @@ const getDivisor = (mass) => {
 };
 
 const getAnswAndQuest = () => {
-  const firstNum = () => number() + 1;
-  const secondNum = () => number() + 1;
-  const question = () => `${firstNum()} ${secondNum()}`;
-  const questionForPlayer = question();
-  const questionForTransmission = questionForPlayer.split(' ');
-  const answer = getDivisor(questionForTransmission);
-  const result = [answer, questionForPlayer];
+  const num = () => number() + 1;
+  const firstNum = num();
+  const secondNum = num();
+  const question = `${firstNum} ${secondNum}`;
+  const answer = getDivisor(firstNum, secondNum);
+  const result = [answer, question];
   return result;
 };
 
