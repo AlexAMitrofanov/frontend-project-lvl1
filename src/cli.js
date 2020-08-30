8485d851a140b5f85e871c588f83c77fe83a1c97
Alex Mitrofanov <alexloves.114@gmail.com>
2020-08-30 15:08:01 +0300

    add getAnswAndQuest func

--- src/cli.js
+++ src/cli.js
@@ -3,3 +3,10 @@ import readlineSinc from 'readline-sync';
 export const name = () => readlineSinc.question('May I have your name? ');
 
 export const number = () => Math.floor(Math.random() * 100);
+
+export const getAnswAndQuest = (func) => {
+  const question = number();
+  const answer = func(question) ? 'yes' : 'no';
+  const result = [answer, question];
+  return result;
+};
