c6d51dd4eb13c31f5cb5dc3a8d57ae058e6d40b6
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-02 21:21:35 +0300

    renamed getName function

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSinc from 'readline-sync';
 
-export const name = () => readlineSinc.question('May I have your name? ');
+export const getName = () => readlineSinc.question('May I have your name? ');
 
 export const getNumberFromGap = (a) => Math.floor(Math.random() * a);
