5c4c0d4c386dfbd6ec1c0ff6e04fd64e6fd70c1d
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-02 21:10:55 +0300

    delete function getNumber

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,4 @@ import readlineSinc from 'readline-sync';
 
 export const name = () => readlineSinc.question('May I have your name? ');
 
-export const getNumber = () => Math.floor(Math.random() * 100);
-
 export const getNumberFromGap = (a) => Math.floor(Math.random() * a);
