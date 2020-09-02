62ab2e0feab046668a0ce7e4b8a17b9f47f3e8ad
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-02 20:41:16 +0300

    deleted func getAnswAndQuest

--- src/cli.js
+++ src/cli.js
@@ -4,11 +4,4 @@ export const name = () => readlineSinc.question('May I have your name? ');
 
 export const getNumber = () => Math.floor(Math.random() * 100);
 
-export const getAnswAndQuest = (func) => {
-  const question = getNumber();
-  const answer = func(question) ? 'yes' : 'no';
-  const result = [answer, question];
-  return result;
-};
-
 export const getNumberFromGap = (a) => Math.floor(Math.random() * a);
