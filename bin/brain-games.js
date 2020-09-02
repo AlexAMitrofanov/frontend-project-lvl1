4db6273d5cb2ac6587d17eca15ef1d794e4492d2
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-02 21:27:06 +0300

    renamed import

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import { name } from '../src/cli.js';
+import { getName } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-const privetstvie = name();
+const privetstvie = getName();
 
 console.log(`Hello, ${privetstvie}!`);
