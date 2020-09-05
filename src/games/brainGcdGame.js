051d2501989ec61eb4d2caaad3481796de21d148
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-05 13:44:29 +0300

    renamed function divisor

--- src/games/brainGcdGame.js
+++ src/games/brainGcdGame.js
@@ -1,18 +1,18 @@
 import { getNumberFromGap } from '../cli.js';
 import playGame from '../index.js';
 
-const getDivisor = (a, b) => {
+const getGreatestCommonDivisor = (a, b) => {
   if (a === 0) {
     return b;
   }
-  let result;
+  let gcd;
   for (let i = a; i > 0; i -= 1) {
     if (a % i === 0 && b % i === 0) {
-      result = i;
+      gcd = i;
       break;
     }
   }
-  return result;
+  return gcd;
 };
 
 const getAnswAndQuest = () => {
@@ -21,7 +21,7 @@ const getAnswAndQuest = () => {
   const firstNum = getNumberFromGap(gapForNumbers, possibleFirstNum);
   const secondNum = getNumberFromGap(gapForNumbers, possibleFirstNum);
   const question = `${firstNum} ${secondNum}`;
-  const answer = getDivisor(firstNum, secondNum);
+  const answer = getGreatestCommonDivisor(firstNum, secondNum);
   const result = [answer, question];
   return result;
 };
