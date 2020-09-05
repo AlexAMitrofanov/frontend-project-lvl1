fd9979307aa35fff8db1a3da8e9b5d92f65203ac
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-05 13:03:41 +0300

    fixed getNunberFromGap function

--- src/cli.js
+++ src/cli.js
@@ -2,4 +2,4 @@ import readlineSinc from 'readline-sync';
 
 export const getName = () => readlineSinc.question('May I have your name? ');
 
-export const getNumberFromGap = (a) => Math.floor(Math.random() * a);
+export const getNumberFromGap = (gap, firstNumber) => Math.floor(Math.random() * gap) + firstNumber;
