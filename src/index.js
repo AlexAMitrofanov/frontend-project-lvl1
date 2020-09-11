1965b2d44dabf6f5a3b50c21fba6c82e6458c1c5
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-11 20:08:33 +0300

    fixed runGameEngine

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import readlineSinc from 'readline-sync';
 
 const runGameEngine = (getAnswerAndQuestion, description) => {
   console.log('Welcome to the Brain Games');
-
   const name = readlineSinc.question('May I have your name? ');
-
   console.log(`Hello, ${name}!`);
-
   console.log(description);
   const playRounds = (round, getAnswAndQuest) => {
     const RoundsQuantity = 3;
@@ -21,16 +18,15 @@ const runGameEngine = (getAnswerAndQuestion, description) => {
     console.log(question);
     const playersAnsw = readlineSinc.question('Your answer: ');
     const programsAnswer = `${answerAndQuestion[0]}`;
-    if (programsAnswer === playersAnsw) {
-      console.log('Correct!');
-      playRounds(round + 1, getAnswAndQuest);
-    }
+    const wrongAnswer = `"${playersAnsw}" is the wrong answer ;(. Correct answer was "${programsAnswer}".`;
+    const suggestionToPlayAgain = `Let's try again, ${name}!`;
     if (programsAnswer !== playersAnsw) {
-      const wrongAnswer = `"${playersAnsw}" is the wrong answer ;(. Correct answer was "${programsAnswer}".`;
-      const suggestionToPlayAgain = `Let's try again, ${name}!`;
       console.log(wrongAnswer);
       console.log(suggestionToPlayAgain);
+      return;
     }
+    console.log('Correct!');
+    playRounds(round + 1, getAnswAndQuest);
   };
 
   return playRounds(0, getAnswerAndQuestion);
