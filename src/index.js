c65be4f463918730578c9972567c55d3d18903ac
Alex Mitrofanov <alexloves.114@gmail.com>
2020-09-17 19:31:02 +0300

    fixed rightAnswer const

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ const runGameEngine = (getAnswerAndQuestion, description) => {
   console.log(`Hello, ${name}!`);
   console.log(description);
   const playRounds = (round, getAnswAndQuest) => {
-    const RoundsQuantity = 3;
+    const roundsQuantity = 3;
     const suxessfullEnd = `Congratulations, ${name}!`;
-    if (round === RoundsQuantity) {
+    if (round === roundsQuantity) {
       console.log(suxessfullEnd);
       return;
     }
-    const [programsAnswer, question] = getAnswAndQuest();
+    const [rightAnswer, question] = getAnswAndQuest();
     console.log(`Question: ${question}`);
-    const playersAnsw = readlineSinc.question('Your answer: ');
-    const wrongAnswer = `"${playersAnsw}" is the wrong answer ;(. Correct answer was "${programsAnswer}".`;
+    const playersAnswer = readlineSinc.question('Your answer: ');
+    const wrongAnswer = `"${playersAnswer}" is the wrong answer ;(. Correct answer was "${rightAnswer}".`;
     const suggestionToPlayAgain = `Let's try again, ${name}!`;
-    if (`${programsAnswer}` !== playersAnsw) {
+    if (rightAnswer !== playersAnswer) {
       console.log(wrongAnswer);
       console.log(suggestionToPlayAgain);
       return;
